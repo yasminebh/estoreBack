b059cfd78b91af2aefd1054377990b7dea30f38d
yasminebh <yasminebharzallah@gmail.com>
2023-12-18 14:08:32 +0100

    adding numeric filters

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -7,7 +7,7 @@ const getAllProductStatic = async (req, res) => {
 };
 
 const getAllProduct = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -19,6 +19,29 @@ const getAllProduct = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
+  //filter by numeric filter on price and ratings 
+if (numericFilters) {
+  const operatorMap = {
+    '>': '$gt',
+    '>=': '$gte',
+    '=': '$eq',
+    '<': '$lt',
+    '<=': '$lte',
+  }
+  const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+  let filters = numericFilters.replace(regEx,(match) => 
+    `-${operatorMap[match]}-`
+  )
+  const options = ['price', 'rating']
+  filters = filters.split(',').forEach(item => {
+    const [field,operator, value] = item.split('-')
+    if(options.includes(field)) {
+      queryObject[field] = {[operator]: Number(value)}
+    }
+  });
+ 
+  console.log(filters)
+}
   console.log(queryObject);
   let result = Product.find(queryObject);
 // sorting our data
